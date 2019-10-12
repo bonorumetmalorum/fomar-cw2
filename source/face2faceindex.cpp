15a4013870d3f9e01b8bfc8eb49f4be03684c283
Govind Venkatesh <venkateshgovind1@gmail.com>
2019-10-12 15:10:13 +0100

    reading in all vertices

--- source/face2faceindex.cpp
+++ source/face2faceindex.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+struct Vert{
+    float x = 0;
+    float y = 0;
+    float z = 0;
+};
 
 int main(int argc, char **argv){
     ifstream triangleSoup;
     triangleSoup.open(argv[1]);
-    long noTriangle = 0;
-    triangleSoup >> noTriangle;
-    cout << "number of traingles in file: " << noTriangle << endl;
+    long nTriangles = 0;
+    triangleSoup >> nTriangles;
+    cout << "number of traingles in file: " << nTriangles << endl;
+    long nVertices = 3*nTriangles;
+    vector<Vert> vertices(nVertices);
+    for(int i = 0; i < nVertices; i++){
+        triangleSoup >> vertices[i].x >> vertices[i].y >> vertices[i].z;
+    }
+    for(Vert v : vertices){
+        cout << v.x << " " << v.y << " " << v.z << endl;
+    }
 }
